9bbde449a8a19f8901e9cf1456ef8cdaf259af5e
Roberto Romero Guadarrama <robertoromeroguadarrama@gmail.com>
2025-10-22 23:05:24 -0600

    Contantes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { Saludar } from './hello/application/Saludo/Saludar';
 import { HelloController } from './hello/infrastructure/http/controllers/hello.controller';
 import { HelloRepositoryImpl } from './hello/infrastructure/implements/hello-repository.impl';
+import { HELLO_REPOSITORY } from './hello/infrastructure/constants';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { HelloRepositoryImpl } from './hello/infrastructure/implements/hello-rep
   controllers: [HelloController],
   providers: [
     Saludar,
-    { provide: 'HelloRepository', useClass: HelloRepositoryImpl },
+    { provide: HELLO_REPOSITORY, useClass: HelloRepositoryImpl },
   ],
 })
 export class AppModule {}
--- src/hello/application/Saludo/Saludar.ts
+++ src/hello/application/Saludo/Saludar.ts
@@ -1,11 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import type { HelloRepository } from 'src/hello/domain/repository/HelloRepository';
 import { Saludo } from 'src/hello/domain/types/Saludo';
+import { HELLO_REPOSITORY } from 'src/hello/infrastructure/constants';
 
 @Injectable()
 export class Saludar {
   constructor(
-    @Inject('HelloRepository') private readonly repository: HelloRepository,
+    @Inject(HELLO_REPOSITORY)
+    private readonly repository: HelloRepository,
   ) {}
 
   get(): Saludo {
