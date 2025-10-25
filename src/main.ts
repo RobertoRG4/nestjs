baa372501e0bcaac309d5df78df82f415f001606
Roberto Romero Guadarrama <robertoromeroguadarrama@gmail.com>
2025-10-25 14:57:17 -0600

    Fix swagger ui

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prefix = process.env.NEXT_CONTEXT || '';
-
-  app.setGlobalPrefix(`${prefix}/api/v1/`);
-  app.useGlobalInterceptors(new ResponseInterceptor());
   const config = new DocumentBuilder()
     .setTitle('Api Generict')
     .setDescription('Simple api generic')
     .setVersion('1.0')
     .build();
+
+  app.setGlobalPrefix(`${prefix}/api/v1/`);
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(`${prefix}/api/v1/swagger-ui/`, app, documentFactory);
 
   await app.listen(process.env.NEXT_PORT ? +process.env.NEXT_PORT : 3000);
 }
-bootstrap().catch((e) => console.log(e));
+bootstrap().catch((e) => console.error(e));
